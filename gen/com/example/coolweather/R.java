1400c58dbddfef420104d229c467e4a2b6d8f9fd
wflyljf <wfly265310@163.com>
2016-06-17 16:16:41 +0800

    修改程序图标和名称。

--- gen/com/example/coolweather/R.java
+++ gen/com/example/coolweather/R.java
@@ -2023,7 +2023,8 @@ containing a value of this type.
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020049;
         public static final int abc_textfield_search_material=0x7f02004a;
         public static final int ic_launcher=0x7f02004b;
-        public static final int notification_template_icon_bg=0x7f02004c;
+        public static final int logo=0x7f02004c;
+        public static final int notification_template_icon_bg=0x7f02004d;
     }
     public static final class id {
         public static final int action0=0x7f090050;
